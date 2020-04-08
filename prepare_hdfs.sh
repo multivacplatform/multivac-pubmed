d1740d0812815ef533ed2de66cad100f5ee89da9
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-04-08 21:30:25 +0200

    Update prepare_hdfs.sh

--- prepare_hdfs.sh
+++ prepare_hdfs.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "cleaning /tmp/pubmed on HDFS"
-hadoop fs -rm -r -skipTrash /tmp/pubmed
-echo "creating /tmp/pubmed on HDFS"
-hadoop fs -mkdir /tmp/pubmed/
 echo "copying data/ to /tmp/pubmed/ on HDFS"
 hadoop fs -put data/tp.ncbi.nlm.nih.gov/pubmed/updatefiles/*.gz /tmp/pubmed/
 echo "copying target/scala-2.11/*.jar to /tmp/pubmed on HDFS"
