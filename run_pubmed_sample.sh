151856b04f74bad4b182c10f28e6b7e03a35eacd
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-04-09 09:43:06 +0200

    Create run_pubmed_sample.sh

---
+++ run_pubmed_sample.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "start downloading updated articles"
+cd "$(dirname "$0")"
+./download_daily.sh
+echo "start pulling latest code, cleaning and creating fat JAR"
+./build_lib.sh
+#change the following base on your environment: local / cluster
+echo "start preparing everything for HDFS"
+./prepare_hdfs.sh
+echo "start Spark job on YARN cluster"
+./start_spark.sh -m yarn -i hdfs:///tmp/pubmed/*.gz -o hdfs:///path-to-output -j hdfs:///tmp/pubmed/multivac-pubmed-assembly-1.0.0.jar
